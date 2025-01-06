990134fcfcf1410ae37599d8974cb6fe8a2b2514
squirrelRao <hqraop@163.com>
2025-01-06 17:10:25 +0800

    Update steel_dataset.py

--- datasets/steel_dataset.py
+++ datasets/steel_dataset.py
@@ -319,7 +319,7 @@ def classify_provider(
 
 if __name__ == "__main__":
     data_folder = "datasets/Steel_data"
-    df_path = "datasets/Steel_data/train.csv"
+    df_path = "/kaggle/input/steel-detection-datasets/train.csv"
     mean = (0.485, 0.456, 0.406)
     std = (0.229, 0.224, 0.225)
     batch_size = 12
